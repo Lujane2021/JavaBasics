1b1f63bf0d3d05629fa33ca8ca102e6b50a521f0
Lujane <lujane.sul@outlook.com>
2022-06-22 02:46:44 +0300

    JavaBasics challenge 2/3 - COMPLETE

---
+++ src/com/suliman/challenge1/Main.java
@@ -0,0 +1,13 @@
+package com.suliman.challenge1;
+
+public class Main {
+    public static void main(String[] args) {
+        String firstname = "Lujane";
+        String lastname ="Suliman";
+
+        String fullname = firstname +" "+ lastname;
+
+        System.out.println(fullname);
+
+    }
+}
